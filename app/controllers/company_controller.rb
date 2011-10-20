523ff8e61751ca37423fc1616d6754bd381e5db6
Joe Kutner <jpkutner@gmail.com>
2011-10-20 16:30:39 -0500

    made the statuses live

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,8 +1,11 @@
 class CompanyController < ApplicationController
+  include TwitterUtil
 
   def index
-    @statuses = (Twitter.user_timeline("codefinger")).map do |status|
-      {:created_at => status.created_at, :status_text => status.text}
+    with_twitter_account do |username|
+      @statuses = (Twitter.user_timeline(username)).map do |status|
+        {:created_at => status.created_at, :status_text => status.text}
+      end
     end
   end
 
