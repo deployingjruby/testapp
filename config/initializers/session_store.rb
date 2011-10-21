55050003236c5be5b3f738dd95bf9b2f310c6a82
Joe Kutner <jpkutner@gmail.com>
2011-10-21 13:53:36 -0500

    switch a couple 1.9 syntax to 1.8

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Twitalytics::Application.config.session_store :cookie_store, key: '_twitalytics_session'
+Twitalytics::Application.config.session_store :cookie_store, :key => '_twitalytics_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
