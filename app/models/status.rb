0665ffaf3f9e08fecb7d258e9d370248b31369e8
Joe Kutner <jpkutner@gmail.com>
2011-11-15 16:01:33 -0600

    brought the customer feed online

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,2 +1,16 @@
 class Status < ActiveRecord::Base
+
+  def self.find_or_create_from(tweets)
+    tweets.map do |tweet|
+      existing = Status.where(:remote_id => tweet.id_str).first
+      existing.nil? ?
+          Status.create(
+            :created_at => tweet.created_at,
+            :status_text => tweet.text,
+            :creator => tweet.from_user,
+            :remote_id => tweet.id_str
+          ) :
+          existing
+    end
+  end
 end
--- db/migrate/20111112225446_create_statuses.rb
+++ db/migrate/20111112225446_create_statuses.rb
@@ -3,7 +3,8 @@ class CreateStatuses < ActiveRecord::Migration
     create_table :statuses do |t|
       t.string :status_text
       t.string :creator
-
+      t.string :remote_id, :unique => true
+      
       t.timestamps
     end
   end
