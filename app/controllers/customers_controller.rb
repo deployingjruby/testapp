8101fca2c83555f968d174bac8d46e1cae6940de
Joe Kutner <jpkutner@gmail.com>
2012-01-01 21:12:28 -0600

    added retweet feature

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,4 +6,9 @@ class CustomersController < ApplicationController
         fetch_recent_tweets(20)
     )
   end
+
+  def retweet
+    Status.find(params[:id]).retweet
+    redirect_to customers_path
+  end
 end
